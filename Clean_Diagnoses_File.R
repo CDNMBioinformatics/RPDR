48ce56e7727ade351dd56c4eec86ffe8ae6ff16b
resta <resta@channing.harvard.edu>
2020-10-26 19:41:15 -0400

    Fixed date display error, made cutoff fn usable without compare fn

--- Clean_Diagnoses_File.R
+++ Clean_Diagnoses_File.R
@@ -32,6 +32,7 @@ process_diagnoses <- function(DF_to_fill = All_merged,
   if(write_files){
     output_file_header <- str_c(output_file_header, "Dia_")
   }
+  Diagnoses <- Diagnoses %>% mutate(Date = mdy(Date)) %>% arrange(EMPI, Date)
   # Get the "Any exist" first to lower the search group/increase speed later
   for (Grouping_Name in names(Diagnoses_Of_Interest)){
     Group_Header = str_c("Any_", gsub(" ", "_", Grouping_Name))
@@ -82,7 +83,7 @@ process_diagnoses <- function(DF_to_fill = All_merged,
         }
         rm(Subgroup2)
         
-        Output_Columns <- Subgroup %>% mutate(Date = mdy(Date)) %>% arrange(Date) %>%
+        Output_Columns <- Subgroup %>% arrange(Date) %>%
           summarise(!!(as.symbol(Subgroup_Header)) := "Yes",
                     !!(as.symbol(str_c(Subgroup_Header, "_total_dates"))) := n(),
                     !!(as.symbol(str_c(Subgroup_Header, "_first_date"))) := first(Date),
@@ -118,7 +119,7 @@ process_diagnoses <- function(DF_to_fill = All_merged,
     }
     Group <- distinct(Group, EMPI, Date, Diagnosis_Name, Hospital, .keep_all = TRUE)
     if (Group_Info){
-      Output_Columns <- Group %>% group_by(EMPI) %>% mutate(Date = mdy(Date)) %>% arrange(Date) %>%
+      Output_Columns <- Group %>% group_by(EMPI) %>% arrange(Date) %>%
         summarise(!!(as.symbol(str_c(Group_Header, "_total_diagnoses"))) := n(),
                   !!(as.symbol(str_c(Group_Header, "_first_date"))) := first(Date),
                   !!(as.symbol(str_c(Group_Header, "_most_recent_date"))) := last(Date),
@@ -137,7 +138,7 @@ process_diagnoses <- function(DF_to_fill = All_merged,
   }
   if(!missing(Merge_Group_Info_Name)){
     Merge_Header <- str_c("Any_", gsub(" ", "_", Merge_Group_Info_Name))
-    Output_Columns <- Relevant_rows %>% mutate(Date = mdy(Date)) %>% arrange(Date) %>% group_by(EMPI) %>%
+    Output_Columns <- Relevant_rows %>% arrange(Date) %>% group_by(EMPI) %>%
       summarise(!!(as.symbol(str_c(Merge_Header))) := "Yes",
                 !!(as.symbol(str_c(Merge_Header, "_total_diagnoses"))) := n(),
                 !!(as.symbol(str_c(Merge_Header, "_first_date"))) := first(Date),
@@ -178,6 +179,19 @@ process_diagnoses_date_cutoff <- function(DF_to_fill_cutoff,
     logerror("No list of Diagnoses were specified. Process stopped.")
     return(DF_to_fill_cutoff)
   }
+  # Usually this would go in the function but because the compare function doesn't like preassigned information, we do this here
+  if (missing(DF_to_fill_cutoff)) {DF_to_fill_cutoff = All_merged}
+  if (missing(input_file_header_cutoff)) {input_file_header_cutoff = config$rpdr_file_header}
+  if (missing(input_file_ending_cutoff)) {input_file_ending_cutoff = config$rpdr_file_ending}
+  if (missing(path_dia_abn_cutoff)) {path_dia_abn_cutoff = str_c(config$data_dir, "Diagnoses_abnormalities/")}
+  if (missing(Exact_cutoff)) {Exact_cutoff = FALSE}
+  if (missing(Individual_Info_cutoff)) {Individual_Info_cutoff = TRUE}
+  if (missing(Group_Info_cutoff)) {Group_Info_cutoff = TRUE}
+  if (missing(write_files_cutoff)) {write_files_cutoff = config$create_intermediates}
+  if (missing(output_file_header_cutoff)) {output_file_header_cutoff = config$intermediate_files_dir}
+  if (missing(output_file_ending_cutoff)) {output_file_ending_cutoff = config$general_file_ending}
+  if (missing(restrict_to_before_cutoff)) {restrict_to_before_cutoff = FALSE}
+  
   loginfo("Processing diagnoses file...")
   Diagnoses <- data.table(fread(str_c(input_file_header_cutoff, "Dia", input_file_ending_cutoff))) %>% 
     arrange(EMPI, Date)
@@ -199,11 +213,16 @@ process_diagnoses_date_cutoff <- function(DF_to_fill_cutoff,
   # Restrict ids to only the ones in the reduced id list
   Diagnoses <- Diagnoses %>% filter(EMPI %in% DF_to_fill_cutoff$EMPI)
   Diagnoses <- Diagnoses %>% mutate(Date = mdy(Date)) %>% arrange(EMPI, Date)
-  EMPI_Date_Limit <- DF_to_fill_cutoff %>% select(EMPI, date_variable_cutoff) %>%
-    rename(Cutoff_Date_Time = date_variable_cutoff) %>%
-    extract(Cutoff_Date_Time, c("Cutoff_Date", "Cutoff_Time"),
-            regex = "(\\d{4}-\\d{2}-\\d{2}) (\\d{2}:\\d{2})", remove = FALSE) %>%
-    mutate(Cutoff_Date = ifelse(is.na(Cutoff_Time), Cutoff_Date_Time, Cutoff_Date)) %>% select(EMPI, Cutoff_Date)
+  if (!sum(str_count(DF_to_fill_cutoff %>% pull(date_variable_cutoff)) == 10) == nrow(DF_to_fill_cutoff)){
+    EMPI_Date_Limit <- DF_to_fill_cutoff %>% select(EMPI, date_variable_cutoff) %>%
+      rename(Cutoff_Date_Time = date_variable_cutoff) %>%
+      extract(Cutoff_Date_Time, c("Cutoff_Date", "Cutoff_Time"),
+              regex = "(\\d{4}-\\d{2}-\\d{2}) (\\d{2}:\\d{2})", remove = FALSE) %>%
+      mutate(Cutoff_Date = ifelse(is.na(Cutoff_Time), Cutoff_Date_Time, Cutoff_Date)) %>% select(EMPI, Cutoff_Date)
+  } else {
+    EMPI_Date_Limit <- DF_to_fill_cutoff %>% select(EMPI, date_variable_cutoff) %>%
+      rename(Cutoff_Date = date_variable_cutoff)
+  }
   # Restrict Diagnoses to only before or only after
   Diagnoses <- left_join(Diagnoses, EMPI_Date_Limit, by = "EMPI")
   time_str = ifelse(restrict_to_before_cutoff, "before", "after")
@@ -272,7 +291,7 @@ process_diagnoses_date_cutoff <- function(DF_to_fill_cutoff,
         }
         rm(Subgroup2)
         
-        Output_Columns <- Subgroup %>% mutate(Date = mdy(Date)) %>% arrange(Date) %>%
+        Output_Columns <- Subgroup %>% arrange(Date) %>%
           summarise(!!(as.symbol(Subgroup_Header)) := "Yes",
                     !!(as.symbol(str_c(Subgroup_Header, "_total_dates"))) := n(),
                     !!(as.symbol(str_c(Subgroup_Header, "_first_date"))) := first(Date),
